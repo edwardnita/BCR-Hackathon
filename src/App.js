1be04ed45a3752b3f1be9ae3986c16aa136f2cb5
andreilaurentiuradu <117320481+andreilaurentiuradu@users.noreply.github.com>
2023-05-27 09:44:52 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Neata coaie!</h1>
+      <h1>Hai cu hacku ala nebun!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
