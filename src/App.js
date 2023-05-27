53788c26bba55ee9b167edcc31e9660270c2fe7e
edwardnita <87010960+edwardnita@users.noreply.github.com>
2023-05-27 10:46:03 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ export default function App() {
   return (
     <div className="main">
       <div className="top">
-        <div className="top_element">de inceput asa</div>
+        <div className="top_element">
+          <div className="logo">
+            de{' '}
+            <img src="https://drive.google.com/uc?export=view&id=1bdtIrzqXasuw_qWBJNDwxKsPvW3zwtYG/"></img>
+          </div>
+        </div>
       </div>
       <div className="content">content</div>
     </div>
