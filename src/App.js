1e50824a252e10fd3a659f660711141e2a17c2b9
edwardnita <87010960+edwardnita@users.noreply.github.com>
2023-05-27 15:08:17 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ export default function App() {
     <div className="main">
       <div className="top">
         <div className="top_element">
-          <img src="https://i.imgur.com/0rzpjwn.png" className="image"></img>
+          <img src="https://i.imgur.com/izJHULq.png" className="image"></img>
         </div>
       </div>
-      <div className="content">content</div>
+      <div className="content">
+        <div className="form">
+          <div className="welcome">
+            <p className="welcome_text">Bine ai revenit!</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
