df9091e5e68ab261c6cd6ec17f2f29858bc2686f
edwardnita <87010960+edwardnita@users.noreply.github.com>
2023-05-27 10:20:08 +0300

    3 files modified

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './style.css';
 
 export default function App() {
   return (
-    <div class="main">
-      <div>
-        <h1>de inceput asa</h1>
+    <div className="main">
+      <div className="top">
+        <div className="top_element">de inceput asa</div>
       </div>
+      <div className="content">content</div>
     </div>
   );
 }
